3c33f2f37ae3572119c2481c888813f9492996b2
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-11-03 18:55:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", renderData)
   // your code here
-});
+  const arr = ["pay bills", "study for exam", "clean bathroom"]
+  const taskForm = document.getElementById('create-task-form')
+
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const formInput = e.target.children[1]
+    renderItem(formInput.value)
+    formInput.value = ""
+  })
+
+  function renderData(){
+    for(element in arr){
+      renderItem(arr[element])
+    }
+  }
+
+  function renderItem(item){
+    const listUl = document.getElementById('tasks')
+    const itemLi = document.createElement('li')
+    itemLi.innerHTML = item
+    listUl.appendChild(itemLi)
+  }
