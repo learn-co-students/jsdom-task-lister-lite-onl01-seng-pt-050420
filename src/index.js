07252ac6ebf176b1a224f2bf2d98776da4bc169f
morgancollado <Morgan.collado@gmail.com>
2020-11-05 20:14:22 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+const ul = document.getElementById("tasks");
+const listItem = document.createElement("li")
+const form = document.getElementById("create-task-form")
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let input = document.getElementById('new-task-description');
+    listItem.innerText = input.value
+    ul.appendChild(listItem)
+     e.target.reset()
+  })
+
+
+
 });
+
+
