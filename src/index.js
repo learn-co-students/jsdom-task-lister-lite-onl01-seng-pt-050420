850cb741b427a3978472fd6e655190b1702bcf17
MisterAnthropy <tjcroley@outlook.com>
2020-11-09 12:33:17 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector('create-task-form');
+
+  taskForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    let inputTag = document.querySelector('input#new-task-description');
+    let inputValue = inputTag.value;
+
+    let newLi = document.createElement('li');
+    newLi.innerText = inputValue;
+
+    let ulTag = document.querySelector('ul#tasks');
+    ulTag.append(newLi);
+    inputTag.value = '';
+  })
 });
