c80cbf0fc1e13e2ca85649c3f0b9ef34880dd3d1
wtwerner <wtwerner@gmail.com>
2020-11-12 22:38:36 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.getElementById("submit-button").addEventListener("click", function(event) {
+    let input = document.getElementById("new-task-description").value;
+    let ul = document.getElementById("tasks");
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(input));
+    ul.appendChild(li);
+    event.preventDefault();
+
+  }, false);
+
 });
