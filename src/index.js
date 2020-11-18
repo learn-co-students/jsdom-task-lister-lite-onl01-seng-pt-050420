e2a85092d569986680eba56e5a12613b01c9451e
kanu-e <chukwuemekakanu@gmail.com>
2020-11-17 23:05:04 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
   // get the info from the textbox and create a new li 
     const list = document.createElement("li")
     const toDo = document.createTextNode(document.getElementById("new-task-description").value)
+    console.log(list.priority)
   // adding and handling 'done' button
     const finished = document.createElement("input")
     finished.value = "Complete" 
-    finished.type = "submit"
-    finished.addEventListener("click", function(e) {
+    finished.addEventListener("click", function() {
       list.remove()
     })     
-  // const priority = document.getElementsByName('task-priority')
-
+  const priority = document.getElementsByName('task-priority')
+  for(let i = 0; i < priority.length; i++) {
+    if (priority[i].checked) {
+      list.classList.add(priority[i].value)
+    }
+  }
   // add the new task to the tasks ul
     list.appendChild(toDo)
     list.appendChild(finished)
