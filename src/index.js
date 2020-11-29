d9223da8dbaccfa064cb50d25837cb9086cd8bb2
Pwako11 <akolo@hotmail.com>
2020-11-29 14:13:26 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  haltSubmit()
 });
+
+function haltSubmit(){
+  document.getElementById('id-submit').addEventListener('click', function(event) {
+          document.getElementById("list").innerHTML += "Sorry! <code>preventDefault()</code> won't let you click this!<br>"
+          event.preventDefault();
+}, false);
+
+}
