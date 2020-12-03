dd38be46161efe1974ef17e846e25087c59bebc5
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-12-03 15:18:01 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+
+  const form = document.getElementById('create-task-form') 
+  
+  form.addEventListener('submit', e => { 
+    e.preventDefault()
+
+      const inputValue = document.getElementById('new-task-description').value
+      //console.log(inputValue) 
+
+      const newLi = document.createElement('li') 
+      newLi.innerText = inputValue 
+
+      const ulTag = document.getElementById('tasks') 
+      ulTag.appendChild(newLi)   
+       
+   }) 
+
+
+
+    
+    
+
+  
