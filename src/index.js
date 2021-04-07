a0af9c144772bd8036b7f0be48aa468d9f276305
Pwako11 <akolo@hotmail.com>
2021-04-07 06:10:59 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const newTaskDescription = document.getElementById("new-task-description");
   const newTaskPriority = document.getElementById("new-task-priority");
   
-  //ul where new tasks will live on the DOM
-  const newTaskUl = document.getElementById("tasks");
-
    //attach event listeners
    newTaskForm.addEventListener("submit", createNewTask);
   
@@ -20,11 +17,26 @@ const createNewTask = event => {
   const newTaskDescription = document.getElementById("new-task-description");
   const newTask = document.createElement("li");
   newTask.innerText = newTaskDescription.value;
+  // newTask.appendChild(button)
 
   appendNewTask(newTask);
   event.target.reset();
 };
 
 const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
+  //ul where new tasks will live on the DOM
+  const newTaskUl = document.getElementById("tasks");
+  const newli = newTaskUl.appendChild(task);
+  const button = document.createElement('button')
+  button.setAttribute('class', 'delete')
+  button.innerHTML = "X"
+  button.addEventListener("click", deleteTask);
+  newli.appendChild(button)
 };
+
+
+const deleteTask = (event) => {
+  event.preventDefault()
+
+  event.target.parentElement.remove()
+}
